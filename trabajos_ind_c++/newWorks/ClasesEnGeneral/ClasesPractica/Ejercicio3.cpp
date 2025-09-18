45e9a52ed4807a8f4972311f1c6a42cf2f95bb79
maaaxxx <morenomaximiliano522@gmail.com>
2025-09-17 20:22:14 -0600

    lmao, listo ej 3

--- trabajos_ind_c++/newWorks/ClasesEnGeneral/ClasesPractica/Ejercicio3.cpp
+++ trabajos_ind_c++/newWorks/ClasesEnGeneral/ClasesPractica/Ejercicio3.cpp
@@ -18,11 +18,22 @@ class Estudiante : public Persona{
   string correoInst; 
   float nota; 
   public: 
-  Estudiante (string, int, string, float); 
-  float Promedio (float); 
+  Estudiante (string, int, string); 
+  int PromedioAcumulado (int); 
   void mostrarEst(); 
   
 }; 
+
+class Empleado : public Persona {
+   private: 
+   float sueldo; 
+   public: 
+   Empleado(string, int, float); 
+
+   void mostrarEmpleado(); 
+
+
+};
 
 
 
@@ -49,38 +60,72 @@ void Persona::Ine() {
     cout<<"Edad: "<<GetAge()<<endl; 
 }
 
+Empleado::Empleado(string _nombre, int _edad, float _sueldo) {
+   setPersona(_nombre, _edad); 
+   sueldo = _sueldo; 
+}
 
-Estudiante::Estudiante(string _nombre, int _edad, string _correo, float _nota) {
- setPersona(_nombre, _edad); 
- correoInst = _correo; 
- nota = _nota; 
+void Empleado::mostrarEmpleado() {
+    Ine(); 
+    cout<<"Sueldo: "<<sueldo<<endl; 
 }
 
 
 
 
-float Estudiante::Promedio(float prom) {
+Estudiante::Estudiante(string _nombre, int _edad, string _correo) {
+ setPersona(_nombre, _edad); 
+ correoInst = _correo; 
+}
+
+
+
+
+int Estudiante::PromedioAcumulado(int n) {
+  int a[100], prom = 0, suma = 0; 
+
+
+  for (int i = 0; i  <n; i++) {
+    cout<<"Dime la nota del estudiante: "<<"["<<i<<"]"; 
+    cin>>a[i]; 
+    suma += a[i]; 
+  }
+ prom = suma/n; 
+
+ cout<<"promedio acumulado: "<<prom<<endl; 
+
+ return prom;  
 
 }
 
 void Estudiante::mostrarEst() {
     Ine(); 
     cout<<"correo inst: "<<correoInst<<endl; 
-    cout<<"Promedio: "<<nota<<endl;
+    PromedioAcumulado(5); 
+
     cout<<endl;  
 
 }
 
 int main () {
       Persona p1; 
-      Estudiante a1("Max", 19, "A01669359", 8.8); 
-      cout<<"----Clase hija estudiambre----"<<endl; 
-      a1.mostrarEst(); 
+      Estudiante a1("Max", 19, "A01669359"); 
+      Empleado e1("Litzi", 25, 1800); 
 
+
+      
     //Persona q es clase padre 
     cout<<"----Clase persona---- "<<endl; 
     p1.setPersona("mOKO", 19); 
     p1.Ine(); 
+
+    cout<<"---Clase Empleado---"<<endl; 
+    e1.mostrarEmpleado(); 
+
+    cout<<"----Clase hija estudiambre----"<<endl; 
+      a1.mostrarEst(); 
+
+
       
 
     return 0;
